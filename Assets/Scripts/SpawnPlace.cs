58613675d9517a365ee52478932eca913c7ebdae
Peter <punisherpete99@gmail.com>
2024-12-03 22:34:29 +0300

    modify spawner

--- Assets/Scripts/SpawnPlace.cs
+++ Assets/Scripts/SpawnPlace.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -6,44 +5,49 @@ using UnityEngine;
 public class SpawnPlace : MonoBehaviour
 {
     [SerializeField] private List<SpawnPlace> neighbours;
-
+    public List<SpawnPlace> Neighbours => neighbours;
+    
+    
     private bool _spawnAllowed = true;
     public bool SpawnAllowed => _spawnAllowed;
 
     private void Start()
     {
-        Physics.OverlapSphere(transform.position + new Vector3(transform.localScale.x/2, 0, 0), 2).ToList()
-            .Where(x =>
-            {
-                Debug.Log("ss");
-                bool result = x.TryGetComponent<SpawnPlace>(out var spawnPlace) &&
-                              spawnPlace != GetComponent<SpawnPlace>();
-                if (result) neighbours.Add(spawnPlace);
-                return result;
-            });
-        Physics.OverlapSphere(transform.position - new Vector3(transform.localScale.x, 0, 0), 0.2f).ToList()
-            .Where(x =>
-            {
-                bool result = x.TryGetComponent<SpawnPlace>(out var spawnPlace) &&
-                              spawnPlace != GetComponent<SpawnPlace>();
-                if (result) neighbours.Add(spawnPlace);
-                return result;
-            });
-        Physics.OverlapSphere(transform.position + new Vector3(0, 0, transform.localScale.z), 0.2f).ToList()
-            .Where(x =>
-            {
-                bool result = x.TryGetComponent<SpawnPlace>(out var spawnPlace) &&
-                              spawnPlace != GetComponent<SpawnPlace>();
-                if (result) neighbours.Add(spawnPlace);
-                return result;
-            });
-        Physics.OverlapSphere(transform.position + new Vector3(0, 0, -transform.localScale.z), 0.2f).ToList()
-            .Where(x =>
-            {
-                bool result = x.TryGetComponent<SpawnPlace>(out var spawnPlace) &&
-                              spawnPlace != GetComponent<SpawnPlace>();
-                if (result) neighbours.Add(spawnPlace);
-                return result;
-            });
+        Vector3[] directions = 
+        {
+            Vector3.right, 
+            Vector3.left, 
+            Vector3.forward, 
+            Vector3.back
+        };
+
+        float checkDistance = transform.localScale.x * 0.5f + 0.1f;
+
+        foreach (var dir in directions)
+        {
+            CheckNeighbour(dir * checkDistance);
+        }
     }
+
+    private void CheckNeighbour(Vector3 offset)
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position + offset, 0.1f);
+
+        foreach (var hitCollider in hitColliders)
+        {
+            if (hitCollider.TryGetComponent<SpawnPlace>(out var spawnPlace) && 
+                spawnPlace != this && 
+                !neighbours.Contains(spawnPlace))
+            {
+                neighbours.Add(spawnPlace);
+            }
+        }
+    }
+
+    public void ChangeSpawnAllowed(bool value)
+    {
+        _spawnAllowed = value;
+    }
+    
+    
 }
--- Assets/Scripts/SpawnerTest.cs
+++ Assets/Scripts/SpawnerTest.cs
@@ -1,41 +1,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using Random = Unity.Mathematics.Random;
 
 public class SpawnerTest : MonoBehaviour
 {
     [SerializeField] private Transform _spawnPointsHolder;
-    
+
     [SerializeField] private GameObject _spawnerPrefab;
     [SerializeField] private GameObject _goldFarmPrefab;
     [SerializeField] private GameObject _shootingTowerPrefab;
 
     private List<SpawnPlace> _spawnPlaces = new List<SpawnPlace>();
-    
+
     private void Start()
     {
         _spawnPlaces = _spawnPointsHolder.GetComponentsInChildren<SpawnPlace>().ToList();
-        
         FulfillGameField();
     }
-    
-    
 
     private void FulfillGameField()
     {
         foreach (var spawnPlace in _spawnPlaces)
         {
-            if (UnityEngine.Random.Range(0, 101) < 51)
+            if (spawnPlace.SpawnAllowed && NoBuildingsNearby(spawnPlace))
             {
                 Spawn(spawnPlace);
+                MarkNeighboursOccupied(spawnPlace);
             }
         }
     }
 
+    private bool NoBuildingsNearby(SpawnPlace spawnPlace)
+    {
+        return spawnPlace.Neighbours.All(neighbour => neighbour.SpawnAllowed);
+    }
+
     private void Spawn(SpawnPlace spawnPlace)
     {
-        Instantiate(GetRandomBuilding(), spawnPlace.transform.position, Quaternion.identity);
+        var building = Instantiate(GetRandomBuilding(), spawnPlace.transform.position, Quaternion.identity);
+        spawnPlace.ChangeSpawnAllowed(false);
+    }
+
+    private void MarkNeighboursOccupied(SpawnPlace spawnPlace)
+    {
+        foreach (var neighbour in spawnPlace.Neighbours)
+        {
+            neighbour.ChangeSpawnAllowed(false); 
+        }
     }
 
     private GameObject GetRandomBuilding()
