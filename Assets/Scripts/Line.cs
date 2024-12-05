14df6b284c87ce5caecdd969f227725b52b40b50
Тимофей <trutimka3105@gmail.com>
2024-12-05 11:42:00 +0300

    update linemanager

---
+++ Assets/Scripts/Line.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    [SerializeField] private GameObject pointA;
+    [SerializeField] private GameObject pointB;
+    
+    public GameObject GetPointA() => pointA;
+    public GameObject GetPointB() => pointB;
+    
+    private LineRenderer lineRenderer; // Компонент для рендеринга линии
+    
+    public event Action OnLineCreated;
+    
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 2;
+        lineRenderer.startWidth = lineRenderer.endWidth = 1.5f;
+    }
+
+    public void Init(GameObject startPoint)
+    {
+        pointA = startPoint;
+        
+        lineRenderer.SetPosition(0, pointA.transform.position);
+    }
+
+    public void FinishLine(GameObject endPoint)
+    {
+        var closestPoints = FindClosestExits(pointA, endPoint); // Находим ближайшие выходы
+        pointA = closestPoints.Item1; // Перезаписываем pointA
+        pointB = closestPoints.Item2; // Перезаписываем pointB
+
+        lineRenderer.SetPosition(0, pointA.transform.position);
+        lineRenderer.SetPosition(1, pointB.transform.position);
+
+        OnLineCreated?.Invoke();
+    }
+
+    private (GameObject, GameObject) FindClosestExits(GameObject buildingA, GameObject buildingB)
+    {
+        if (buildingA == null || buildingB == null) return (null, null);
+
+        // Получаем все выходы для обоих зданий
+        var exitsA = buildingA.GetComponentsInChildren<Transform>()
+            .Where(t => t != buildingA.transform) // Исключаем сам объект здания
+            .Select(t => t.gameObject)
+            .ToList();
+
+        var exitsB = buildingB.GetComponentsInChildren<Transform>()
+            .Where(t => t != buildingB.transform)
+            .Select(t => t.gameObject)
+            .ToList();
+
+        if (exitsA.Count == 0 || exitsB.Count == 0) return (buildingA, buildingB); // Если нет выходов, соединяем здания
+
+        GameObject closestA = null;
+        GameObject closestB = null;
+        float minDistance = float.MaxValue;
+
+        // Перебираем все пары выходов между двумя зданиями
+        foreach (var exitA in exitsA)
+        {
+            foreach (var exitB in exitsB)
+            {
+                float distance = Vector3.Distance(exitA.transform.position, exitB.transform.position);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    closestA = exitA;
+                    closestB = exitB;
+                }
+            }
+        }
+
+        return (closestA, closestB); // Возвращаем пару ближайших выходов
+    }
+}
--- Assets/Scripts/LineManager.cs
+++ Assets/Scripts/LineManager.cs
@@ -5,13 +5,82 @@ using Object = UnityEngine.Object;
 
 public class LineManager : MonoBehaviour
 {
-    private Dictionary<Tuple<Object, Object>, LineRenderer> _lines;
-    private LineRenderer currLineRenderer;
-    private void Awake()
+    [SerializeField] private LayerMask buildingMask;
+    [SerializeField] private LayerMask spawnMask;
+    [SerializeField] private Line prefabLine;
+    private List<Line> lines = new List<Line>();
+    private Line currentLine;
+
+    private void Update()
     {
-        // Получаем компонент LineRenderer
-        currLineRenderer = GetComponent<LineRenderer>();
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (currentLine != null)
+            {
+                lines.Remove(currentLine);
+                Destroy(currentLine);
+                currentLine = null;
+            }
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 10000, spawnMask))
+            {
+                var pointA = hit.transform.parent == null ? hit.transform.gameObject : hit.transform.parent.gameObject;
+                currentLine = Instantiate(prefabLine, Vector3.zero, Quaternion.identity, transform);
+                currentLine.Init(pointA);
+                
+                Debug.Log(hit.transform.name);
+                Debug.Log("hit");
+            }
+            
+        }
+        
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (currentLine == null) return;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, 10000, buildingMask))
+            {
+                Vector3 direction = hit.point - currentLine.GetPointA().transform.position;
+                float distance = direction.magnitude;
+                
+                var hits = Physics.RaycastAll(currentLine.GetPointA().transform.position, direction.normalized, distance, buildingMask);
+                var obj1 = hit.transform.parent == null ? hit.transform.gameObject : hit.transform.parent.gameObject;
+                foreach (var rayHit in hits)
+                {
+                    var obj = rayHit.transform.parent == null ? rayHit.transform.gameObject : rayHit.transform.parent.gameObject;
+                    if (obj != currentLine.GetPointA() && obj != obj1)
+                    {
+                        Destroy(currentLine.gameObject);
+                        currentLine = null;
+                        return;
+                    }
+                }
+                var pointB = obj1;
+                currentLine.FinishLine(pointB);
+                lines.Add(currentLine); // Добавляем в список
+                Debug.Log($"Линия завершена: {currentLine.GetPointA().name} -> {currentLine.GetPointB().name}");
+
+                currentLine = null; // Сбрасываем текущую линию
+            }
+            else
+            {
+                // Если конечная точка не найдена, уничтожаем текущую линию
+                Destroy(currentLine.gameObject);
+                currentLine = null;
+            }
+        }
+
+        if (Input.GetMouseButton(0) && currentLine != null)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, 10000))
+            {
+                currentLine.GetComponent<LineRenderer>()
+                    .SetPosition(1, hit.point);
+            }
+        }
     }
-    
-    
 }
